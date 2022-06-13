74c790df69fd4e1f0b83946687fa33cc4651e75b
Ivantubik <ivanharalampie@gmail.com>
2022-06-13 14:58:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".menu");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
